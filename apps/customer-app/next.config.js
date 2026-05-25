4c62504484f41bcec7c5970fd6f074a0459f33d0
Aman Mandal <67774794+AmanMandaL-noob@users.noreply.github.com>
2026-05-25 16:33:45 +0530

    driver next.js update

--- apps/customer-app/next.config.js
+++ apps/customer-app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // <-- This tells Next.js to generate the static 'out' folder
 };
 
 export default nextConfig;
